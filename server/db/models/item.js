47c974780c5ec7e3a3968237b215c9990cea295d
Eliana Miller <elianasmiller@gmail.com>
2020-03-02 17:25:35 -0500

    added Item modelin database

---
+++ server/db/models/item.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Item = db.define('item', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  imageUrl: {
+   type: Sequelize.STRING
+},
+  quantity: {
+    type: Sequelize.INTEGER
+  },
+  tags: {
+    type: Sequelize.ARRAY()
+  }
+})
+
+module.exports = Item
