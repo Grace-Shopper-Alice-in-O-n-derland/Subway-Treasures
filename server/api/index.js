8ef000ef2f3e9719695cc9589e6b6a2722f11f66
Carrie Pascale <52431501+cpascale43@users.noreply.github.com>
2020-03-03 11:01:09 -0500

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-router.use('./items', require('./items'))
+router.use('/items', require('./items'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
